7b9eab2ed8e1637fc98bff2b8b022d6c50d5f5f2
Edit Olah <edit.olah.01@gmail.com>
2019-02-08 10:04:23 +0000

    Add passport and passport-google-oauth.

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const keys = require('./config/keys');
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'right there, right there - Flashpoint!' });
-});
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    }, 
+    accessToken => {
+      console.log(accessToken);
+    }
+  )
+);
+
+app.get(
+  '/auth/google', 
+  passport.authenticate('google', {
+    scope: ['https://www.googleapis.com/auth/plus.login']
+  })
+);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
